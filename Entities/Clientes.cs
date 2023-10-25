59b15fd9fe790b371508460f8ebd3c716ae26d15
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2023-10-25 09:09:06 -0300

    tabela clientes

---
+++ Entities/Clientes.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPai.Entities
+{
+    public class Clientes
+    {
+        public int Id{get;set;}
+        public string Nome{get;set;}
+        public string Telefone{get;set;} 
+        public string QuadraELote{get;set;}
+        public string Bairro{get;set}
+    }
+}
